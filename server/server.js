c05c3587c5a1e776c587c309e3cb21dca3f561ae
Hamzakhan-git <hamzakhan91827@gmail.com>
2025-07-30 17:27:31 +0530

    add server

--- server/server.js
+++ server/server.js
@@ -24,6 +24,6 @@ app.use('/api/user',userRouter)
 
 const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, ()=>{
-//     console.log("Server is running on port",PORT);
-// })
+app.listen(PORT, ()=>{
+    console.log("Server is running on port",PORT);
+})
